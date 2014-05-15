01044096bfa49f9efca6e071850132ba9b897deb
refaelos <refaeldakar@gmail.com>
2014-05-15 13:30:13 +0300

    - Added Copyright message template to it can be easily added in intellij - Added docs for Mission class

--- SoomlaAndroidBlueprint/src/com/soomla/blueprint/challenges/Mission.java
+++ SoomlaAndroidBlueprint/src/com/soomla/blueprint/challenges/Mission.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2012-2014 Soomla Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.soomla.blueprint.challenges;
 
 import com.soomla.blueprint.data.BPJSONConsts;
@@ -17,26 +33,45 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by refaelos on 13/05/14.
+ * A Mission is a task your users needs to complete in your game. Missions are usually associated
+ * with rewards meaning that you can give your users something for completing them.
+ * You can create missions and use them as single, independent, entities OR you can create a
+ * <code>Challenge</code> to handle several missions and monitor their completion.
  */
 public abstract class Mission {
-    private static final String TAG = "SOOMLA Mission";
-    private String mMissionId;
-    private String mName;
-    private List<Reward> mRewards;
 
+    /**
+     * Constructor.
+     *
+     * @param name is the name of the mission (something you might want to display on the screen.
+     * @param missionId is the id of the mission
+     */
     public Mission(String name, String missionId) {
         mName = name;
         mMissionId = missionId;
         mRewards = new ArrayList<Reward>();
     }
-
+    
+    /**
+     * Constructor.
+     *
+     * @param name is the name of the mission (something you might want to display on the screen.
+     * @param missionId is the id of the mission.
+     * @param rewards is the rewards that you want to give your users on mission completion.
+     */
     public Mission(String missionId, String name, List<Reward> rewards) {
         mMissionId = missionId;
         mName = name;
         mRewards = rewards;
     }
 
+    /**
+     * Constructor.
+     * Generates an instance of <code>Mission</code> from the given <code>JSONObject</code>.
+     *
+     * @param jsonObject A JSONObject representation of the wanted <code>Mission</code>.
+     * @throws JSONException
+     */
     public Mission(JSONObject jsonObject) throws JSONException {
         mMissionId = jsonObject.getString(BPJSONConsts.BP_MISSION_MISSIONID);
         mName = jsonObject.getString(BPJSONConsts.BP_NAME);
@@ -56,6 +91,11 @@ public abstract class Mission {
         }
     }
 
+    /**
+     * Converts the current <code>Mission</code> to a JSONObject.
+     *
+     * @return A <code>JSONObject</code> representation of the current <code>Mission</code>.
+     */
     public JSONObject toJSONObject(){
         JSONObject jsonObject = new JSONObject();
         try {
@@ -73,35 +113,40 @@ public abstract class Mission {
         return jsonObject;
     }
 
-    public List<Reward> getRewards() {
-        return mRewards;
-    }
-
-    public String getName() {
-        return mName;
-    }
-
-    public String getMissionId() {
-        return mMissionId;
-    }
-
+    /**
+     * Check weather the current mission is completed or not.
+     *
+     * @return the completion status of the current mission.
+     */
     public boolean isCompleted() {
         return MissionsStorage.isCompleted(this);
     }
 
+    /**
+     * Use this function to force the completion status of the mission.
+     * The completion status will be saved to the database.
+     * In case of a successful completion, the associated rewards will be given.
+     *
+     * @param completed is the completion status you want to set to the mission.
+     */
     public void setCompleted(boolean completed) {
         MissionsStorage.setCompleted(this, completed);
         if (completed) {
-            giveRewards();
+
+            // The mission is completed, giving the rewards.
+            for(Reward reward : mRewards) {
+                reward.give();
+            }
         }
     }
 
-    private void giveRewards() {
-        for(Reward reward : mRewards) {
-            reward.give();
-        }
-    }
-
+    /**
+     * Checks if the given Object is equal to this Mission, by comparing the given object's
+     * mission id with this <code>Mission</code>'s missionId.
+     *
+     * @param o the object to compare
+     * @return true if the objects are equal, otherwise false
+     */
     @Override
     public boolean equals(Object o) {
         if (o instanceof Mission) {
@@ -111,8 +156,37 @@ public abstract class Mission {
         return false;
     }
 
+    /**
+     * Returns the hashCode of <code>mMissionId</code> if it exists
+     *
+     * @return the hashCode of <code>mMissionId</code>
+     */
     @Override
     public int hashCode() {
         return this.mMissionId.hashCode();
     }
+
+
+    /** Setters and Getters **/
+
+    public String getMissionId() {
+        return mMissionId;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public List<Reward> getRewards() {
+        return mRewards;
+    }
+
+
+    /** Private Members **/
+
+    private static final String TAG = "SOOMLA Mission";
+
+    private String mMissionId;
+    private String mName;
+    private List<Reward> mRewards;
 }

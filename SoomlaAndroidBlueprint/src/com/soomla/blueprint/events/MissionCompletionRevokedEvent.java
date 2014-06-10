8dc23a47cf85232c9781b454df28e15a87d3d72f
lassic <homedev@gmail.com>
2014-06-10 15:37:34 +0300

    add 2 requested events

---
+++ SoomlaAndroidBlueprint/src/com/soomla/blueprint/events/MissionCompletionRevokedEvent.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2012-2014 Soomla Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.soomla.blueprint.events;
+
+import com.soomla.blueprint.challenges.Mission;
+
+/**
+ * This event is fired when <code>Mission</code> completion has been revoked.
+ * For example, you can decide to revoke a mission if the condition for completing
+ * it is no longer valid.
+ */
+public class MissionCompletionRevokedEvent {
+
+    /** read-only Properties **/
+
+    public final Mission Mission;
+
+    /**
+     * Constructor
+     * @param mission to be revoked
+     */
+    public MissionCompletionRevokedEvent(Mission mission) {
+        Mission = mission;
+    }
+}

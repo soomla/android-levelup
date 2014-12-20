a94e97e3666d88b3fbf610ae7e2496d571b72d6f
refaelos <refaeldakar@gmail.com>
2014-12-20 14:14:36 +0200

    added functions for times-completed for levels

--- SoomlaAndroidLevelUp/src/com/soomla/levelup/data/LevelStorage.java
+++ SoomlaAndroidLevelUp/src/com/soomla/levelup/data/LevelStorage.java
@@ -45,6 +45,10 @@ public class LevelStorage {
         return keyLevels(levelId, "played");
     }
 
+    private static String keyTimesCompleted(String worldId) {
+        return keyLevels(worldId, "timesCompleted");
+    }
+
     private static String keySlowestDuration(String levelId) {
         return keyLevels(levelId, "slowest");
     }
@@ -154,6 +158,40 @@ public class LevelStorage {
 
     public static int getTimesPlayed(String levelId) {
         String key = keyTimesPlayed(levelId);
+        String val = KeyValueStorage.getValue(key);
+        return TextUtils.isEmpty(val) ? 0 : Integer.parseInt(val);
+    }
+
+    /**
+     * World Times Completed *
+     */
+
+    public static int incTimesCompleted(String worldId) {
+        int completed = getTimesCompleted(worldId);
+        if (completed < 0) { /* can't be negative */
+            completed = 0;
+        }
+        String completedStr = "" + (completed + 1);
+        String key = keyTimesCompleted(worldId);
+        KeyValueStorage.setValue(key, completedStr);
+
+        return completed + 1;
+    }
+
+    public static int decTimesCompleted(String worldId) {
+        int completed = getTimesCompleted(worldId);
+        if (completed <= 0) { /* can't be negative or zero */
+            return 0;
+        }
+        String completedStr = "" + (completed - 1);
+        String key = keyTimesCompleted(worldId);
+        KeyValueStorage.setValue(key, completedStr);
+
+        return completed - 1;
+    }
+
+    public static int getTimesCompleted(String worldId) {
+        String key = keyTimesCompleted(worldId);
         String val = KeyValueStorage.getValue(key);
         return TextUtils.isEmpty(val) ? 0 : Integer.parseInt(val);
     }

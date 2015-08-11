5a219c16a9ee9bfc09191fdfb40db1595e7656f0
Boris Spektor <boris@soom.la>
2015-08-11 14:55:34 +0300

    Added version and rebuilt jars

--- SoomlaAndroidLevelUp/src/com/soomla/levelup/LevelUp.java
+++ SoomlaAndroidLevelUp/src/com/soomla/levelup/LevelUp.java
@@ -45,6 +45,7 @@ import java.util.List;
 public class LevelUp {
 
     public static final String DB_KEY_PREFIX = "soomla.levelup.";
+    public static final String VERSION = "1.0.8";
 
     public static JSONObject getLevelUpState() {
         JSONObject stateJSON = new JSONObject();
